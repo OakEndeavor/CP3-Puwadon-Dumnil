d6be475a630e83632a7c794bf00c1da55d5ae9f2
OakEndeavor <95973619+OakEndeavor@users.noreply.github.com>
2021-12-20 02:56:01 +0700

    Add Points
    Points : FE,GB,ICS,CP Points

---
+++ Exercise4_Puwadon_D.py
@@ -0,0 +1,9 @@
+print("-----Your Score-----")
+FEPoints  = 90
+GBPoints  = 95
+ICSPoints = 100
+CPPoints  = 100
+print("Foundation English                :" ,FEPoints)
+print("General Business                  :" ,GBPoints)
+print("Introduction to Computer System   :" ,ICSPoints)
+print("Computer Programming              :" ,CPPoints)
