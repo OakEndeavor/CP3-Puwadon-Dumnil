ab176fe58fcf9ec95e9c7b71c9a5487734484481
OakEndeavor <95973619+OakEndeavor@users.noreply.github.com>
2021-12-21 03:27:00 +0700

    Added information
    information = distance,time,velocity

---
+++ Exercise5_2_Puwadon_D.py
@@ -0,0 +1,9 @@
+print("Python Program to calculate velocity")
+Distance = float(input("Please Enter Distance(>1km)  :"))
+Time = float(input("Please Enter Time(>1hr)  :"))
+print("------------result------------")
+velocity = Distance/Time
+print("velocity  :",velocity,"in km/hr unit")
+print("and")
+velocity = velocity*5/18
+print("velocity  :",velocity,"in m/s unit")
